f2ee4cc019297780e8d3a75f6cdc97103e7438be
Michel Oliveira <mr.micheloliveira@gmail.com>
2021-01-28 15:37:19 -0300

    concluindo creates

--- creates.sql
+++ creates.sql
@@ -3,3 +3,18 @@ CREATE TABLE membro(
     nome VARCHAR(40) NOT NULL,
     idade INT CHECK(idade > 0) NOT NULL
 );
+
+CREATE TABLE livro(
+    isbn BIGINT PRIMARY KEY,
+    titulo TEXT NOT NULL,
+    autores VARCHAR(40) NOT NULL,
+    editora VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE emprestado(
+    num_membro INT PRIMARY KEY,
+    isbn BIGINT PRIMARY KEY,
+    data DATE NOT NULL,
+    FOREIGN KEY num_membro REFERENCES membro(num_membro),
+    FOREIGN KEY isbn REFERENCES livro(isbn)
+);
