74ebf4986fcb5f07736fee3bb4ac4b29ba6f373d
Michel Oliveira <mr.micheloliveira@gmail.com>
2021-01-28 16:25:52 -0300

    adicionando insert membros

---
+++ insert_membros.sql
@@ -0,0 +1,14 @@
+INSERT INTO membro (num_membro, nome, idade) VALUES
+(854, 'Smith', 51),
+(125, 'Carlos Andrade', 32),
+(258, 'Maria Lima', 26),
+(247, 'João Silva', 40),
+(698, 'Romeu Lopes', 21),
+(741, 'Carina Nunes', 18),
+(627, 'Luana Souza', 12),
+(551, 'Rafael Menezes', 35),
+(773, 'Olivia Santos', 29),
+(323, 'Elis Prado', 54)
+(445, 'Paulo Dias', 62),
+(245, 'Michel Oliveira', 23),
+(966, 'Iara Moraes', 57);
