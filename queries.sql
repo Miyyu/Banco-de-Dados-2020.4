cbbe62c1851c4959085b3e99b83126f0c4d231cf
Michel Oliveira <mr.micheloliveira@gmail.com>
2021-01-29 20:04:35 -0300

    Adicionado query C

--- queries.sql
+++ queries.sql
@@ -7,6 +7,13 @@ HAVING COUNT (e.num_membro) > (SELECT COUNT(e.num_membro)
                               FROM emprestado e JOIN membro m ON m.num_membro = e.num_membro
                               WHERE m.nome = 'Smith');
 
+--Consulta c) Liste os membros maiores de 50 anos e que fizeram pelo menos 3 empréstimos.
+
+SELECT Distinct m.nome
+FROM  membro m,emprestado e
+WHERE m.idade > 50 and (SELECT COUNT(*)
+        FROM emprestado e
+        WHERE e.num_membro = m.num_membro) >= 3;
 
 --Consulta e) Ache os membros que não fizeram empréstimos de livros do autor Abraham.
 
